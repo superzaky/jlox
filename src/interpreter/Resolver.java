d24f045b09807f388b8ce542b8da2c0457e1d365
superzaky <superkytoz@gmail.com>
2024-09-11 23:11:20 +0200

    Properties on instances and Methods on classes

--- src/interpreter/Resolver.java
+++ src/interpreter/Resolver.java
@@ -16,7 +16,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     
     private enum FunctionType {
       NONE,
-      FUNCTION
+      FUNCTION,
+      METHOD
     }
 
     @Override
@@ -31,6 +32,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitClassStmt(Stmt.Class stmt) {
       declare(stmt.name);
       define(stmt.name);
+      for (Stmt.Function method : stmt.methods) {
+        FunctionType declaration = FunctionType.METHOD;
+        resolveFunction(method, declaration);
+      }
+
       return null;
     }
 
@@ -122,6 +128,19 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         resolve(argument);
       }
   
+      return null;
+    }
+
+    @Override
+    public Void visitGetExpr(Expr.Get expr) {
+      resolve(expr.object);
+      return null;
+    }
+
+    @Override
+    public Void visitSetExpr(Expr.Set expr) {
+      resolve(expr.value);
+      resolve(expr.object);
       return null;
     }
 
